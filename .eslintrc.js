abe839321a50c85073c1a03b4a96a172e5a3f5c4
Daniel Tedman <dbtedman@gmail.com>
2020-09-06 15:41:01 +1000

    Dependency updates + Docs cleanup + Release prep

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
-//------------------------------------------------------------------------------
-//
-// ESLint Configuration, https://eslint.org/docs/user-guide/configuring
-//
-// Based on https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md
-//
-//  - https://www.npmjs.com/package/@typescript-eslint/eslint-plugin
-//  - https://www.npmjs.com/package/@typescript-eslint/parser
-//  - https://www.npmjs.com/package/eslint-config-prettier
-//  - https://www.npmjs.com/package/eslint-plugin-jest
-//  - https://www.npmjs.com/package/eslint-plugin-promise
-//  - https://www.npmjs.com/package/eslint-plugin-security-node
-//
-//------------------------------------------------------------------------------
-
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
