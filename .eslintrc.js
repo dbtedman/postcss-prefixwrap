a490cefd5c487540457b4b347cfdc9172f128a9f
Daniel Tedman <dbtedman@gmail.com>
2021-05-01 19:16:51 +1000

    Deps updates + minor cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
+    "prettier",
     "plugin:jest/recommended",
     "plugin:security-node/recommended",
     "plugin:promise/recommended",
