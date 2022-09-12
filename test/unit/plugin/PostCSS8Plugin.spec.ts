e0ec48619e4d559e2bced51b3f58ef1a686920fe
Daniel Tedman <dbtedman@gmail.com>
2022-09-12 10:33:14 +1000

    Expand test coverage

--- test/unit/plugin/PostCSS8Plugin.spec.ts
+++ test/unit/plugin/PostCSS8Plugin.spec.ts
@@ -1,7 +1,19 @@
 import { asPostCSSv8PluginGenerator } from "../../../src/plugin/PostCSS8Plugin";
+import { PostCSSContainer, PostCSSRule } from "../../../src/Types";
 
 describe("asPostCSSv8PluginGenerator", () => {
   it("can be generated", () => {
-    expect(asPostCSSv8PluginGenerator()(".my-custom-wrap", {})).not.toBeNull();
+    const plugin = asPostCSSv8PluginGenerator()(".my-custom-wrap", {});
+    const walkRules = jest.fn((callback) => {
+      const atRule: PostCSSRule = {
+        selector: ".something",
+        walkRules: jest.fn(),
+      };
+      callback(atRule);
+    });
+
+    plugin.Once({ walkRules } as PostCSSContainer);
+
+    expect(walkRules).toHaveBeenCalledTimes(1);
   });
 });
