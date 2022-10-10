f2616df42bada6209d230cacb1b771c9cf597685
Daniel Tedman <dbtedman@gmail.com>
2022-10-10 18:27:36 +1000

    Initial refactor for integration tests

---
+++ test/integration/less/build.js
@@ -0,0 +1,13 @@
+const source = "p { color: red; }";
+const expected = ".my-selector p { color: red; }";
+
+const result = require("postcss")([
+  require("postcss-prefixwrap")(".my-selector"),
+]).process(source);
+
+// TODO: Not testing with less yet
+
+if (result.css !== expected) {
+  console.error(`[${result.css}] did not match expected [${expected}]`);
+  process.exit(1);
+}
