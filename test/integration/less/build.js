421292506bff77a29808f5923ed9503c7a8fec8e
Daniel Tedman <dbtedman@gmail.com>
2022-10-18 19:30:13 +1000

    Fix less integration test

--- test/integration/less/build.js
+++ test/integration/less/build.js
@@ -1,11 +1,17 @@
-const source = "p { color: red; }";
-const expected = ".my-selector p { color: red; }";
+const fs = require("fs");
+const path = require("path");
+
+const source = fs.readFileSync(path.join(__dirname, "compiled.css"), {
+  encoding: "utf8",
+});
+const expected =
+  ".my-custom-wrap > :not(.something) .Something .other {\n" +
+  "  width: 100%;\n" +
+  "}\n";
 
 const result = require("postcss")([
-  require("postcss-prefixwrap")(".my-selector"),
+  require("postcss-prefixwrap")(".my-custom-wrap > :not(.something)"),
 ]).process(source);
-
-// TODO: Not testing with less yet
 
 if (result.css !== expected) {
   console.error(`[${result.css}] did not match expected [${expected}]`);
